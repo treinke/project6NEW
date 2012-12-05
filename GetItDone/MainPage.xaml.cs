b74582d8c2afce3ebd60c2053ce8936437306131
Larry Schneck <larry.y2k13@gmail.com>
2012-12-04 22:37:50 -0800

    socket class!!!!!

--- GetItDone/MainPage.xaml.cs
+++ GetItDone/MainPage.xaml.cs
@@ -470,6 +470,13 @@ namespace GetItDone
         //Sync events with the server
         private void syncButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            string address ="lore.cs.purdue.edu";
+            const int BACKUP_PORT = 7272;
+            SocketClient client = new SocketClient();
+            client.Connect(address, BACKUP_PORT);
+            client.Send(remList.returnAll());
+            client.Receive();
+            client.Close();
             //connect to server
             //check for pending requests
             //if pending requests
@@ -478,10 +485,8 @@ namespace GetItDone
                 //recreate remList from string
                 //add reminder if it doesn't already exist
                 //clear events from screen
-                //add event to screen
-                
+                //add event to screen  
         }
-
     }
     //Node Class
     public class Node
@@ -1071,7 +1076,159 @@ namespace GetItDone
         }
         return "none";
     }
-}
+    }
+    public class SocketClient
+    {
+        Socket _socket = null;
+        static ManualResetEvent _clientDone = new ManualResetEvent(false);
+        const int TIMEOUT_MILLISECONDS = 5000;
+        const int MAX_BUFFER_SIZE = 2048;
+        public SocketClient(){
+        }
+        public string Receive()
+        {
+            string response = "Operation Timeout";
+
+            // We are receiving over an established socket connection
+            if (_socket != null)
+            {
+                // Create SocketAsyncEventArgs context object
+                SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+                socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+
+                // Setup the buffer to receive the data
+                socketEventArg.SetBuffer(new Byte[MAX_BUFFER_SIZE], 0, MAX_BUFFER_SIZE);
+
+                // Inline event handler for the Completed event.
+                // Note: This even handler was implemented inline in order to make this method self-contained.
+                socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate(object s, SocketAsyncEventArgs e)
+                {
+                    if (e.SocketError == SocketError.Success)
+                    {
+                        // Retrieve the data from the buffer
+                        response = Encoding.UTF8.GetString(e.Buffer, e.Offset, e.BytesTransferred);
+                        response = response.Trim('\0');
+                    }
+                    else
+                    {
+                        response = e.SocketError.ToString();
+                    }
+
+                    _clientDone.Set();
+                });
+
+                // Sets the state of the event to nonsignaled, causing threads to block
+                _clientDone.Reset();
+
+                // Make an asynchronous Receive request over the socket
+                _socket.ReceiveAsync(socketEventArg);
+
+                // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+                // If no response comes back within this time then proceed
+                _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+            }
+            else
+            {
+                response = "Socket is not initialized";
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Closes the Socket connection and releases all associated resources
+        /// </summary>
+        public void Close()
+        {
+            if (_socket != null)
+            {
+                _socket.Close();
+            }
+        }
+        public string Send(string data)
+        {
+            string response = "Operation Timeout";
+
+            // We are re-using the _socket object that was initialized in the Connect method
+            if (_socket != null)
+            {
+                // Create SocketAsyncEventArgs context object
+                SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+
+                // Set properties on context object
+                socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+                socketEventArg.UserToken = null;
+
+                // Inline event handler for the Completed event.
+                // Note: This event handler was implemented inline in order to make this method self-contained.
+                socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate(object s, SocketAsyncEventArgs e)
+                {
+                    response = e.SocketError.ToString();
+
+                    // Unblock the UI thread
+                    _clientDone.Set();
+                });
+
+                // Add the data to be sent into the buffer
+                byte[] payload = Encoding.UTF8.GetBytes(data);
+                socketEventArg.SetBuffer(payload, 0, payload.Length);
+
+                // Sets the state of the event to nonsignaled, causing threads to block
+                _clientDone.Reset();
+
+                // Make an asynchronous Send request over the socket
+                _socket.SendAsync(socketEventArg);
+
+                // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+                // If no response comes back within this time then proceed
+                _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+            }
+            else
+            {
+                response = "Socket is not initialized";
+            }
+
+            return response;
+        }
+
+        public string Connect(string hostName, int portNumber)
+        {
+            string result = string.Empty;
+
+            // Create DnsEndPoint. The hostName and port are passed in to this method.
+            DnsEndPoint hostEntry = new DnsEndPoint(hostName, portNumber);
+
+            // Create a stream-based, TCP socket using the InterNetwork Address Family. 
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Create a SocketAsyncEventArgs object to be used in the connection request
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+            socketEventArg.RemoteEndPoint = hostEntry;
+
+            // Inline event handler for the Completed event.
+            // Note: This event handler was implemented inline in order to make this method self-contained.
+            socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate(object s, SocketAsyncEventArgs e)
+            {
+                // Retrieve the result of this request
+                result = e.SocketError.ToString();
+
+                // Signal that the request is complete, unblocking the UI thread
+                _clientDone.Set();
+            });
+
+            // Sets the state of the event to nonsignaled, causing threads to block
+            _clientDone.Reset();
+
+            // Make an asynchronous Connect request over the socket
+            _socket.ConnectAsync(socketEventArg);
+
+            // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+            // If no response comes back within this time then proceed
+            _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+
+            return result;
+        }
+    }
 }
 
 
